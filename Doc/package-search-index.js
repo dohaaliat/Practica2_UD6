b17b5fdb950a75ed3ca46edca7895340419a224d
Doha Aliat Belkas <dohaaliat0@gmail.com>
2023-05-02 16:43:02 +0200

    Tarea2

---
+++ Doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ed.doha"},{"l":"es.cipfpbatoi.ed.practica2_ud6"}];updateSearchResults();
---
+++ Doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.cipfpbatoi.ed.practica2_ud6","l":"AppTest"},{"p":"ed.doha","l":"Calculadora"}];updateSearchResults();
